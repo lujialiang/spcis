fbfae36ad170e19730abafc4bcc97d7f7c2c1341
pdt <pault.eg@googlemail.com>
2011-12-28 17:30:02 +0000

    removed modelsim do script, and added ttaskfile supporting modelsim and xilinx-isim

--- ttaskfile.tcl
+++ ttaskfile.tcl
@@ -1,13 +1,14 @@
 #
 # spcis ttaskfile
 #
+config simulator modelsim
 set tests {spcis_test}
 
 task sim {sim build -exe gui -index 1}
 task test {sim build -exe noGui -index 1}
 task clean {rmdir build}
 
-project add sim -type modelsim
-sim src -addRtl rtl/*.vhdl -addSim sim/*.vhdl 
+project add sim -type $simulator
+sim src -add {rtl/*.vhdl sim/*.vhdl} 
 sim build -buildDir build -tests $tests
 
