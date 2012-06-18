25803a7a26178dd66e30cf0d1415c2219c0214c8
pdt <pault.eg@googlemail.com>
2012-06-18 10:29:03 +0100

    updated ttaskfile for latest ttask

--- ttaskfile.tcl
+++ ttaskfile.tcl
@@ -2,7 +2,6 @@
 # spcis ttaskfile
 #
 config simulator modelsim
-set tests {spcis_test}
 
 task sim {sim build -exe gui -index 1}
 task test {sim build -exe noGui -index 1}
@@ -10,5 +9,5 @@ task clean {rmdir build}
 
 project add sim -type $simulator
 sim src -add {rtl/*.vhdl sim/*.vhdl} 
-sim build -buildDir build -tests $tests
+sim build -buildDir build
 
