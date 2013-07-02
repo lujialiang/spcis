169c9af48f5e6d98b548e9e916ff511a2008f4da
pdt <pault.eg@googlemail.com>
2013-07-02 09:51:05 +0100

    changed test to default task in ttaskfile

--- ttaskfile.tcl
+++ ttaskfile.tcl
@@ -1,10 +1,11 @@
 #
 # spcis ttaskfile
 #
+
 config simulator modelsim
 
-task sim {sim build -exe gui -index 1}
 task test {sim build -exe noGui -index 1}
+task sim {sim build -exe gui -index 1}
 task clean {rmdir build}
 
 project add sim -type $simulator
